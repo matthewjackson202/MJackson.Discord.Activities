917a9f5bf4e3e329fae11ab7f21cff006301f9dd
Matthew Jackson <103076064+matthewjackson202@users.noreply.github.com>
2022-04-06 22:35:11 -0400

    Add arguments to CreateBot Activity

--- Bot/Activities/CreateBot.cs
+++ Bot/Activities/CreateBot.cs
@@ -1,10 +1,25 @@
 ﻿using System;
 using System.Activities;
+using System.ComponentModel;
+using Discord.WebSocket;
 
 namespace MJackson.Discord.Activities.Bot.Activities
 {
     public class CreateBot : DiscordActivities
     {
+        
+        [Category("Input")]
+        [RequiredArgument]
+        public InArgument<string> ClientToken { get; set; }
+        
+        [Category("Input")]
+        [RequiredArgument]
+        public InArgument<string> ClientPrefix { get; set; }
+        
+        [Category("Output")]
+        public OutArgument<DiscordSocketClient> FirstNumber { get; set; }
+        
+        
         protected override void Execute(CodeActivityContext context)
         {
             Console.WriteLine("yep?");
