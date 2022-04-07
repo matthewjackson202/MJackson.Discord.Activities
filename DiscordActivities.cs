6913d613057d50754b3969d9fd919a847ed579c0
Matthew Jackson <103076064+matthewjackson202@users.noreply.github.com>
2022-04-06 22:12:40 -0400

    First Push

---
+++ DiscordActivities.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Activities;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace MJackson.Discord.Activities
+{
+    public class DiscordActivities : CodeActivity
+    {
+        protected override void Execute(CodeActivityContext context)
+        {
+            
+        }
+    }
+}
