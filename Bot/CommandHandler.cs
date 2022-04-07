765d101392609b3dac064d235e545019053e4b0e
Matthew Jackson <103076064+matthewjackson202@users.noreply.github.com>
2022-04-06 23:48:21 -0400

    Fill out CommandHandler
    Includes a quick implementation for creating commands, this feature probably won't actually exist for a while though.

--- Bot/CommandHandler.cs
+++ Bot/CommandHandler.cs
@@ -1,7 +1,45 @@
-﻿namespace MJackson.Discord.Activities.Bot
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace MJackson.Discord.Activities.Bot
 {
     public class CommandHandler
     {
-        
+        private readonly DiscordSocketClient _client;
+        private readonly CommandService _commands;
+
+        public CommandHandler(DiscordSocketClient client, CommandService commands)
+        {
+            _commands = commands;
+            _client = client;
+        }
+
+        public async Task InstallCommandsAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+            await _commands.AddModulesAsync(assembly: Assembly.GetEntryAssembly(), services: null);
+        }
+
+        private async Task HandleCommandAsync(SocketMessage messageParam)
+        {
+            var message = messageParam as SocketUserMessage;
+            if (message == null) return;
+
+            var argPos = 0;
+            if (!(message.HasCharPrefix('!', ref argPos) ||
+                  message.HasMentionPrefix(_client.CurrentUser, ref argPos)) || message.Author.IsBot) return;
+
+            var context = new SocketCommandContext(_client, message);
+
+            await _commands.ExecuteAsync
+            (
+                context: context,
+                argPos: argPos,
+                services: null
+            );
+        }
     }
 }
