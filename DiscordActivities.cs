ea9c702c5474fb48b566e2c7c6e5464f9aa1bc37
Matthew Jackson <103076064+matthewjackson202@users.noreply.github.com>
2022-04-06 22:50:26 -0400

    Move CreateBot to main class
    This will be the first activity in your UiPath scope now. Makes more sense this way for now

--- DiscordActivities.cs
+++ DiscordActivities.cs
@@ -1,14 +1,37 @@
 ﻿using System;
 using System.Activities;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
+using Discord.WebSocket;
 
 namespace MJackson.Discord.Activities
 {
-    public class DiscordActivities : CodeActivity
+    public class CreateBot : CodeActivity
     {
+
+        public static DiscordSocketClient _client;
+        
+        [Category("Input")] [RequiredArgument] 
+        public InArgument<string> ClientToken { get; set; }
+
+        [Category("Input")] [RequiredArgument] 
+        public InArgument<string> ClientPrefix { get; set; }
+
+        /*
+        [Category("Output")] 
+        public OutArgument<DiscordSocketClient> DiscordClient { get; set; }*/
+
+
         protected override void Execute(CodeActivityContext context)
         {
+            // Get the application token
+            var token = ClientToken.Get(context);
+
+            //Get command prefix for bot to respond to
+            var prefix = ClientPrefix.Get(context);
+            
+            
             
         }
     }
