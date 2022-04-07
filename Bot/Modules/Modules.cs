182b7369cad7a3a3b18efb5d24ca80866a484254
Matthew Jackson <103076064+matthewjackson202@users.noreply.github.com>
2022-04-06 23:49:51 -0400

    Create AdminModule with two basic commands
    These will be in case I find a situation where the bot will run for a long period of time and needs to be shutdown?

---
+++ Bot/Modules/Modules.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace MJackson.Discord.Activities.Bot.Modules
+{
+    public class Modules
+    {
+        public class AdminModule : ModuleBase<SocketCommandContext>
+        {
+            [Command("say")]
+            [Summary("Echoes message")]
+            public Task SayAsync([Remainder] [Summary("Text to echo")] string echo) => ReplyAsync(echo);
+
+            [Command("stop")]
+            [Summary("Stop the bot")]
+            public Task StopCAsync()
+            {
+                ReplyAsync("Alright. Goodbye! ");
+                Environment.Exit(0);
+                return BotHandler.Client.StopAsync();
+            }
+        }
+    }
+}
