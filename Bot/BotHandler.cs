bf96a40d8f9c3a7864673348a391883650dfcd5f
Matthew Jackson <103076064+matthewjackson202@users.noreply.github.com>
2022-04-06 23:50:18 -0400

    Finish BotHandler class
    Login sequence fully fleshed out now. Sends all bot logs to console for now.

---
+++ Bot/BotHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Activities;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace MJackson.Discord.Activities.Bot
+{
+    public class BotHandler
+    {
+
+        public static DiscordSocketClient Client;
+        
+        public async Task StartAsync(string token)
+        {
+            Client = new DiscordSocketClient();
+            var commandService = new CommandService();
+            Client.Log += Log;
+            
+            await Client.LoginAsync(TokenType.Bot, token);
+            var handler = new CommandHandler(Client, commandService);
+            await handler.InstallCommandsAsync();
+
+            await Client.StartAsync();
+
+        }
+
+        private Task Log(LogMessage logMessage)
+        {
+            Console.WriteLine(logMessage);
+            return Task.CompletedTask;
+        }
+        
+    }
+}
