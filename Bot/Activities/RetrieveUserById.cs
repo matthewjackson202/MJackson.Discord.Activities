515033e7f7be3d13f8f50674906ce1c277450a38
Matthew Jackson <103076064+matthewjackson202@users.noreply.github.com>
2022-04-06 23:50:44 -0400

    Create RetrieveUserById Activity
    This activity allows you to retrieve a user by id from the bot. This will retrieve any user on Discord by their ID

---
+++ Bot/Activities/RetrieveUserById.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Activities;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using Discord;
+
+namespace MJackson.Discord.Activities.Bot.Activities
+{
+    public class RetrieveUserById : CodeActivity
+    {
+        
+        
+        [Category("Input")] [RequiredArgument] 
+        public InArgument<string> UserId { get; set; }
+
+        [Category("Output")] [RequiredArgument] 
+        public OutArgument<string> FoundUser { get; set; }
+
+
+        protected override async void Execute(CodeActivityContext context)
+        {
+
+            var userId = UserId.Get(context);
+            var userLongId = Convert.ToUInt64(userId);
+            
+            var userFound = BotHandler.Client.GetUserAsync(userLongId).Result;
+            
+            Console.WriteLine(userFound);
+            FoundUser.Set(context, userFound.Username);
+        }
+    }
+}
