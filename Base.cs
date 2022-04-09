6c9164099068958f6b7fb8b515bb8d1204c75e0a
Matthew Jackson <103076064+matthewjackson202@users.noreply.github.com>
2022-04-08 21:13:15 -0400

    Remove un-used ClientPrefix fields

--- Base.cs
+++ Base.cs
@@ -12,28 +12,19 @@ namespace MJackson.Discord.Activities
     public class CreateBot : CodeActivity
     {
 
-        public static DiscordSocketClient _client;
+        public static DiscordSocketClient client;
         
         [Category("Input")] [RequiredArgument] 
         public InArgument<string> ClientToken { get; set; }
-
-        [Category("Input")] [RequiredArgument] 
-        public InArgument<string> ClientPrefix { get; set; }
 
         protected override async void Execute(CodeActivityContext context)
         {
             // Get the application token
             var token = ClientToken.Get(context);
-
-            //Get command prefix for bot to respond to
-            var prefix = ClientPrefix.Get(context);
-
+            
             var botHandler = new BotHandler();
             await botHandler.StartAsync(token);
-            _client = BotHandler.Client;
-            
-            await Task.Delay(-1);
-
+            client = BotHandler.Client;
             
         }
     }
