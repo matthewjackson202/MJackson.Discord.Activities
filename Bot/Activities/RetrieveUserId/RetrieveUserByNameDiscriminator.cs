db13cc51dd2244c7631095037a644cf374edf496
Matthew Jackson <103076064+matthewjackson202@users.noreply.github.com>
2022-04-08 23:34:35 -0400

    Implement Retrieve User by Name Discriminator

---
+++ Bot/Activities/RetrieveUserId/RetrieveUserByNameDiscriminator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Activities;
+using System.ComponentModel;
+using System.Linq;
+using Discord;
+using Discord.WebSocket;
+
+namespace MJackson.Discord.Activities.Bot.Activities.RetrieveUserId
+{
+    public class RetrieveUserByNameDiscriminator : CodeActivity
+    {
+        // Setup inputs and outputs
+        [Category("Input")] [RequiredArgument]
+        public InArgument<string> UserName { get; set; }
+
+        [Category("Input")]
+        [RequiredArgument]
+        public InArgument<string> UserDiscriminator { get; set; }
+        
+        [Category("Output")]
+        public OutArgument<string> UserId { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            var bot = CreateBot.client;
+            
+            foreach (var guild in bot.Guilds)
+            {
+                var users = guild.Users.Where(x => x.Username == UserName.Get(context))
+                    .Where(x => x.Discriminator == UserDiscriminator.Get(context)).ToList();
+                if (users.Any())
+                {
+                    UserId.Set(context, users.First().Id.ToString());
+                    return;
+                }
+            }
+            UserId.Set(context, null);
+        }
+    }
+}
